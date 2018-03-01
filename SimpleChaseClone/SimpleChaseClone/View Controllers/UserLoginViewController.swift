7f79c36638839bfc8b56c8aad8868ec855564967
Jermaine Kelly <jerjunkel@gmail.com>
2018-03-01 11:49:19 -0500

    Added background imageview to loginVC

--- SimpleChaseClone/SimpleChaseClone/View Controllers/UserLoginViewController.swift
+++ SimpleChaseClone/SimpleChaseClone/View Controllers/UserLoginViewController.swift
@@ -15,7 +15,30 @@ class UserLoginViewController: UIViewController {
         setUpViewController()
     }
 
+    //MARK:- Utilities
     private func setUpViewController() {
         view.backgroundColor = Colors.blue.color
+        addSubViews()
+        setContraints()
     }
+    
+    private func addSubViews() {
+        view.addSubview(backgroundImage)
+    }
+    
+    private func setContraints() {
+        _ = [backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImage.heightAnchor.constraint(greaterThanOrEqualTo: view.heightAnchor, multiplier: 0.7)
+            ].map{$0.isActive = true}
+    }
+    
+    //MARK: Views
+    private var backgroundImage: UIImageView = {
+        let imageView: UIImageView = UIImageView()
+        imageView.backgroundColor = .yellow
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 }
