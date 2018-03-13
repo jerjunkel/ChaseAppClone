838663989d9a98748e8ddb91c751840776d6080f
Jermaine Kelly <jerjunkel@gmail.com>
2018-03-12 21:58:27 -0400

    Removed textfield extension

--- SimpleChaseClone/SimpleChaseClone/Supporting Files/Extensions.swift
+++ SimpleChaseClone/SimpleChaseClone/Supporting Files/Extensions.swift
@@ -15,24 +15,3 @@ extension UIView {
     }
 }
 
-extension UITextField {
-    func setBottomBorder() {
-        //        self.borderStyle = .none
-        //        self.layer.backgroundColor = UIColor.white.cgColor
-        //
-        //        self.layer.masksToBounds = false
-        //        self.layer.shadowColor = UIColor.gray.cgColor
-        //        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
-        //        self.layer.shadowOpacity = 1.0
-        //        self.layer.shadowRadius = 0.0
-        
-        let border = CALayer()
-        let width = CGFloat(2.0)
-        border.borderColor = UIColor.darkGray.cgColor
-        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
-        
-        border.borderWidth = width
-        self.layer.addSublayer(border)
-        self.layer.masksToBounds = true
-    }
-}
