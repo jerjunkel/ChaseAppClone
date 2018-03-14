340188adda7442184e2e9296b005f7e64f14a495
Jermaine Kelly <jerjunkel@gmail.com>
2018-03-14 15:00:27 -0400

    Added loadingVC

---
+++ SimpleChaseClone/SimpleChaseClone/View Controllers/LoadingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingViewController.swift
+//  SimpleChaseClone
+//
+//  Created by Jermaine Kelly on 3/14/18.
+//  Copyright © 2018 Jermaine Kelly. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
