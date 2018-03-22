7d01512228e63564bf9be048484239556559a67a
Jermaine Kelly <jerjunkel@gmail.com>
2018-03-21 22:45:49 -0400

    Added SlideContainerVC

---
+++ SimpleChaseClone/SimpleChaseClone/View Controllers/SlideContainterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SlideContainterViewController.swift
+//  SimpleChaseClone
+//
+//  Created by Jermaine Kelly on 3/21/18.
+//  Copyright © 2018 Jermaine Kelly. All rights reserved.
+//
+
+import UIKit
+
+class SlideContainterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
