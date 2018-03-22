0f6992764c555bb347b0834e0c2947e7b5f98c24
Jermaine Kelly <jerjunkel@gmail.com>
2018-03-22 18:44:12 -0400

    Added swipe gesture to slideContainer

--- SimpleChaseClone/SimpleChaseClone/View Controllers/SlideContainterViewController.swift
+++ SimpleChaseClone/SimpleChaseClone/View Controllers/SlideContainterViewController.swift
@@ -40,6 +40,7 @@ class SlideContainterViewController: UIViewController {
         view.backgroundColor = .clear
         addMainViewControllerToParent()
         addSidePanelViewControllerToParent()
+        addSwipeGesture()
     }
     
     private func addMainViewControllerToParent() {
@@ -51,7 +52,7 @@ class SlideContainterViewController: UIViewController {
         addChildViewController(centerNavigationController!)
         centerNavigationController!.didMove(toParentViewController: self)
     }
-
+    
     private func addSidePanelViewControllerToParent() {
         guard let leftViewController = sidePanelViewController else { return }
         view.insertSubview(leftViewController.view, at: 0)
@@ -59,40 +60,82 @@ class SlideContainterViewController: UIViewController {
         leftViewController.didMove(toParentViewController: self)
     }
     
+    private func addSwipeGesture() {
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+        centerNavigationController?.view.addGestureRecognizer(panGestureRecognizer)
+    }
+    
     //MARK:- Animation Utilities
     private func animateViewController() {
-        let animator: UIViewPropertyAnimator = UIViewPropertyAnimator(duration: 0.5, dampingRatio: 0.8)
+        let animator: UIViewPropertyAnimator = UIViewPropertyAnimator(duration: 0.5, dampingRatio: 1)
         
         switch currentState {
         case .collapsed:
             animator.addAnimations {
                 self.centerNavigationController?.view.frame.origin.x = self.view.center.x + 50
             }
+            addShadow(true)
             currentState = .expanded
         case .expanded:
             animator.addAnimations {
                 self.centerNavigationController?.view.frame.origin.x = 0
             }
+            animator.addCompletion({ (position) in
+                if position == .end {
+                    self.addShadow(false)
+                }
+            })
             currentState = .collapsed
         }
         
         animator.startAnimation()
     }
     
-    private func addShadow() {
-        switch currentState {
-        case .collapsed:
-            centerNavigationController?.view.layer.shadowOpacity = 0.0
-        case .expanded:
+    private func addShadow(_ shouldShowShadow: Bool) {
+        if shouldShowShadow {
             centerNavigationController?.view.layer.shadowOpacity = 0.8
-            centerNavigationController?.view.layer.shadowRadius = 3
+        }else {
+            centerNavigationController?.view.layer.shadowOpacity = 0.0
         }
     }
 }
 
+//MARK:- CenterViewControllerDelegate Method
 extension SlideContainterViewController: CenterViewControllerDelegate {
     func toggleSlide() {
         animateViewController()
-        addShadow()
+    }
+}
+
+//MARK:- UIGestureRecognizerDelegate Methods
+extension SlideContainterViewController: UIGestureRecognizerDelegate {
+    @objc func handlePanGesture(_ recognizer: UIPanGestureRecognizer) {
+        guard let recognizerView = recognizer.view else { return }
+        let gestureIsDraggingFromLeftToRight = (recognizer.velocity(in: view).x > 0)
+        
+        switch recognizer.state {
+        case .began:
+            addShadow(true)
+        case .changed:
+            if gestureIsDraggingFromLeftToRight {
+                recognizerView.center.x = recognizerView.center.x + recognizer.translation(in: view).x
+                recognizer.setTranslation(CGPoint.zero, in: view)
+            }else {
+                guard currentState != .collapsed && recognizerView.frame.maxX >= view.frame.maxX else { return }
+                recognizerView.center.x = recognizerView.center.x + recognizer.translation(in: view).x
+                recognizer.setTranslation(CGPoint.zero, in: view)
+            }
+        case .ended:
+            let hasMovedGreaterThanHalfway = recognizerView.center.x > view.bounds.size.width
+            if hasMovedGreaterThanHalfway {
+                currentState = .collapsed
+                animateViewController()
+            }else {
+                currentState = .expanded
+                animateViewController()
+            }
+        default:
+            break
+        }
     }
 }
