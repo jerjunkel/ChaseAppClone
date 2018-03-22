b8ffde9ef147fc4033470430078d082afb62c9d6
Jermaine Kelly <jerjunkel@gmail.com>
2018-03-21 22:59:45 -0400

    Added setUp functions to sliderContainerVC

--- SimpleChaseClone/SimpleChaseClone/View Controllers/SlideContainterViewController.swift
+++ SimpleChaseClone/SimpleChaseClone/View Controllers/SlideContainterViewController.swift
@@ -18,11 +18,24 @@ class SlideContainterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpViewController()
     }
     
     convenience init(centerVC: UIViewController, leftVC: UIViewController) {
         self.init()
         centerViewController = centerVC
         leftViewController = leftVC
+    }
+    
+    private func setUpViewController() {
+        view.backgroundColor = .red
+    }
+    
+    //MARK:- Utilities
+    private func addCenterVCToParent() {
+        guard let centerVC = centerViewController else { return }
+        view.addSubview(centerVC.view)
+        addChildViewController(centerVC)
+        centerVC.didMove(toParentViewController: self)
     }
 }
